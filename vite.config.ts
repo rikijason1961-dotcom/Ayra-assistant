1b0812ee9959c311378d4785cad4cbae59eb287f
Riki <riki.jason1961@gmail.com>
2026-01-17 14:09:05 +0530

    [project]

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
